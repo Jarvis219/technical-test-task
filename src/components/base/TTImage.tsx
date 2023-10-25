1328d1fb574216894579372b7be6162e8e2048c4
Tran Anh Quang <quang.tran@kiaisoft.com>
2023-10-25 22:49:50 +0700

    feat(base): create base image

---
+++ src/components/base/TTImage.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import {
+  IMAGE_HEIGHT_DEFAULT,
+  IMAGE_WIDTH_DEFAULT,
+  imageUrl,
+} from '@/constants'
+import { isEmpty } from '@/utils'
+import Image from 'next/image'
+import { memo, useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface ITTImageProps {
+  src: string
+  width?: number
+  height?: number
+  alt?: string
+  className?: string
+  unoptimized?: boolean
+  lazy?: boolean
+  priority?: boolean
+}
+
+const TTImage = ({
+  src = imageUrl.logo,
+  alt = '',
+  className = '',
+  height = IMAGE_HEIGHT_DEFAULT,
+  width = IMAGE_WIDTH_DEFAULT,
+  unoptimized = false,
+  priority = false,
+  lazy = true,
+}: ITTImageProps): JSX.Element => {
+  const classes = twMerge(
+    'aspect-[4/3] object-contain w-auto h-auto',
+    className
+  )
+
+  const [photo, setPhoto] = useState<string>(imageUrl.logo)
+
+  useEffect(() => {
+    isEmpty(src) ? setPhoto(imageUrl.logo) : setPhoto(src)
+  }, [src])
+
+  return (
+    <Image
+      src={photo}
+      alt={alt}
+      width={width}
+      height={height}
+      unoptimized={unoptimized}
+      loading={lazy ? 'lazy' : 'eager'}
+      priority={priority}
+      className={classes}
+      onError={(): void => setPhoto(imageUrl.logo)}
+    />
+  )
+}
+
+export default memo(TTImage)
