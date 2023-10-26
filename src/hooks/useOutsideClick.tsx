45aab1ed8bed0b50232d8a33d574761a6d666ea3
Tran Anh Quang <quang.tran@kiaisoft.com>
2023-10-26 22:34:03 +0700

    feat(hooks): click outside custom hook

---
+++ src/hooks/useOutsideClick.tsx
@@ -0,0 +1,25 @@
+import { RefObject, useEffect, useRef } from 'react'
+
+export const useOutsideClick = (
+  callback: () => void
+): RefObject<HTMLDivElement> => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent): void => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return (): void => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [callback])
+
+  return ref
+}
+
+export default useOutsideClick
