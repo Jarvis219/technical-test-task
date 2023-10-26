f126aa89a19646240ea0deda599005f058ffecae
Tran Anh Quang <quang.tran@kiaisoft.com>
2023-10-26 09:06:03 +0700

    feat(assets): add new image band and usdt

--- src/constants/images.constant.ts
+++ src/constants/images.constant.ts
@@ -1,7 +1,9 @@
-const imagePaths = 'assets/images/'
+const imagePaths = '/assets/images/'
 
 export const imageUrl = {
   logo: `${imagePaths}logo.png`,
+  band: `${imagePaths}band.jpg`,
+  usdt: `${imagePaths}usdt.png`,
 }
 
 export const IMAGE_WIDTH_DEFAULT = 1200
