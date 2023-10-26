f5028cda93d84409ed999aeb6383e1998a7fd5d4
Tran Anh Quang <quang.tran@kiaisoft.com>
2023-10-26 22:39:52 +0700

    feat(portfolio-chart): create portfolio chart

---
+++ src/components/portfolio/PortfolioChart.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { IProfitByDay } from '@/types'
+import { convertDateToMonth } from '@/utils'
+import { memo, useMemo } from 'react'
+import Chart from 'react-apexcharts'
+
+interface IProfitCardProps {
+  profitByDay: IProfitByDay
+}
+
+const PortfolioChart = ({ profitByDay }: IProfitCardProps): JSX.Element => {
+  const labels = Object.keys(profitByDay)
+
+  const options = useMemo(
+    () => ({
+      chart: {
+        id: 'chart',
+        toolbar: {
+          show: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      markers: {
+        size: 0,
+        style: 'hollow',
+      },
+      xaxis: {
+        categories: labels.map(convertDateToMonth),
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.9,
+          opacityTo: 1,
+          stops: [0, 100],
+        },
+      },
+    }),
+    [labels]
+  )
+
+  const series = useMemo(
+    () => [
+      {
+        data: Object.values(profitByDay)?.map((val) => +Number(val).toFixed(2)),
+      },
+    ],
+    [profitByDay]
+  )
+
+  return (
+    <div className='mt-2'>
+      <Chart options={options} series={series} type='area' height={600} />
+    </div>
+  )
+}
+
+export default memo(PortfolioChart)
